fdfa21371b5e05291ff6b34e394283ba34812fb5
iDave7 <56477312+iDave7@users.noreply.github.com>
2020-12-21 19:17:29 +0100

    Nuevo proyecto Alquiler patinetes

---
+++ src/Controlador/Controller.java
@@ -0,0 +1,14 @@
+package Controlador;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void initialize() {
+
+    }
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+        System.out.println("Instrumento ingresado correctamente.");
+    }
+}
